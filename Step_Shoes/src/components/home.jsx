7580564341259693ee68114ad44ab9d2692459bb
Eduardo Ganchala <eduardo.ganchala@epn.edu.ec>
2025-07-28 14:33:30 -0500

    Modulos creados y actualizados

---
+++ Step_Shoes/src/components/home.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Prueba</h1>
+        </div>
+    )
+
+}
+export default Home;
---
+++ Step_Shoes/src/components/servicios.jsx
@@ -0,0 +1,10 @@
+
+function Servicios() {
+  return (
+    <div className="servicios">
+      <h2>Servicios</h2>
+      <p>Ofrecemos una variedad de servicios para satisfacer tus necesidades.</p>
+    </div>
+  );
+}
+export default Servicios;
