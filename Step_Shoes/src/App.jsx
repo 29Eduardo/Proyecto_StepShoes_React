811dabdf8fc47eb597da612a2b6340b817302192
Eduardo Ganchala <eduardo.ganchala@epn.edu.ec>
2025-07-30 11:16:02 -0500

    Cambios

--- Step_Shoes/src/App.jsx
+++ Step_Shoes/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
 import Servicios from './components/Servicios'
+import Nosotros from './components/Nosotros'
+import Galeria from './components/Galeria'
+import Contacto from './components/Contacto'
+import Login from './components/Login'
+import Registro from './components/Registro'
 function App() {
   const [count, setCount] = useState(0)
 
